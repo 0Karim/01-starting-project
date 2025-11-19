0c626720bfdccf1a7f42bf7d645c8097209149d7
karim <karimalaa298@gmail.com>
2025-11-19 13:16:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
+import { useState } from 'react';
+import { EXAMPLES } from './data.js'
+
+
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState('');
+
+  function handleSelect(selectedButton){
+        // console.log(selectedButton);
+        // tabContent = selectedButton;
+        setSelectedTopic(selectedButton);
+        // console.log(tabContent);
+    }
+    
+    let tabContent = <p>Please click a topic.</p>;
+    if(selectedTopic){
+      tabContent = <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+    }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>          
+          <ul>
+            {/* <CoreConcept 
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            /> */}
+
+            {/* <CoreConcept 
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}                        
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}            
+             />                        
+            <CoreConcept 
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}            
+            />                         */}
+
+            {CORE_CONCEPTS.map((conceptitem) => <CoreConcept key={conceptitem.title} {...conceptitem} />)}
+
+
+            {/* <CoreConcept {...CORE_CONCEPTS[0]} />            
+            <CoreConcept {...CORE_CONCEPTS[1]} />             
+            <CoreConcept {...CORE_CONCEPTS[2]} />            
+            <CoreConcept {...CORE_CONCEPTS[3]} />                                     */}
+          </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={selectedTopic == 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic == 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic == 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic == 'state'} onSelect={() => handleSelect('state')}>State</TabButton>                                    
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
